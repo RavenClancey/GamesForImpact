7a4ac91cbd23196c276e09f0707aaf8422d2b4a1
RavenClancey <RavenClancey1@gmail.com>
2020-06-12 02:23:55 +1200

    almost done
    almost done

---
+++ Assets/acceptanceScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class acceptanceScript : MonoBehaviour
+{
+    private int timer = 0;
+
+    public AudioSource sound1;
+    
+
+    public AudioSource sound2;
+
+    public AudioSource sound3;
+
+    public AudioSource sound4;
+
+    private bool played1 = false;
+    private bool played2 = false;
+    private bool played3 = false;
+    private bool played4 = false;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += 1;
+        Debug.Log(timer);
+        if (timer > 100 && played1 == false){
+            sound1.Play();
+            played1 = true;
+        }
+
+        if (timer > 550 && played2 == false)
+        {
+            sound2.Play();
+            played2 = true;
+        }
+
+        if (timer > 800 && played3 == false)
+        {
+            sound3.Play();
+            played3 = true;
+        }
+        if (timer > 1220 && played4 == false)
+        {
+            sound4.Play();
+            played4 = true;
+        }
+    }
+}
---
+++ Assets/fadeout.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class fadeout : MonoBehaviour
+{
+
+    public GameObject player;
+    public Image image;
+    [SerializeField] private bool fade = false;
+
+    private void Update()
+    {
+        if (player.transform.position.x > 217)
+        {
+            fade = true; 
+        }
+
+        if (fade == true)
+        {
+            var tempColor = image.color;
+            tempColor.a += 0.002f;
+            image.color = tempColor;
+
+            player.GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>().m_MaxSpeed = 0;
+        }
+      
+        if (image.color.a > 2)
+        {
+
+            SceneManager.LoadScene("AcceptanceScene");
+        }
+    }
+
+}
